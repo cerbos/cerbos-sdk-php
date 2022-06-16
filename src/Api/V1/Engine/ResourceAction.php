359176db5886840e13e1ba47faaec1fe0fb951e2
Oğuzhan Durgun <oguzhandurgun95@gmail.com>
2022-06-16 15:39:09 +0300

    Add Cerbos.Api.V1.Engine.ResourceAction and Cerbos.Sdk.Builder.ResourceAction classes
    Signed-off-by: Oğuzhan Durgun <oguzhandurgun95@gmail.com>

---
+++ src/Api/V1/Engine/ResourceAction.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Cerbos\Api\V1\Engine;
+
+// Copyright 2021-2022 Zenauth Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+class ResourceAction
+{
+    private Resource $resource;
+    private array $actions;
+
+    /**
+     * @param Resource $resource
+     * @param array $actions array of strings
+     */
+    public function __construct(Resource $resource, array $actions)
+    {
+        $this->resource = $resource;
+        $this->actions = $actions;
+    }
+
+    /**
+     * @return Resource
+     */
+    public function getResource(): Resource
+    {
+        return $this->resource;
+    }
+
+    /**
+     * @param Resource $resource
+     * @return void
+     */
+    public function setResource(Resource $resource): void
+    {
+        $this->resource = $resource;
+    }
+
+    /**
+     * @return array
+     */
+    public function getActions(): array
+    {
+        return $this->actions;
+    }
+
+    /**
+     * @param array $actions array of strings
+     * @return void
+     */
+    public function setActions(array $actions): void
+    {
+        $this->actions = $actions;
+    }
+
+    /**
+     * @param string $kind
+     * @return void
+     */
+    public function setKind(string $kind): void {
+        $this->resource->setKind($kind);
+    }
+
+    /**
+     * @param string $policyVersion
+     * @return void
+     */
+    public function setPolicyVersion(string $policyVersion): void {
+        $this->resource->setPolicyVersion($policyVersion);
+    }
+
+    /**
+     * @param string $key
+     * @param string $value
+     * @return void
+     */
+    public function setAttribute(string $key, string $value): void{
+        $a = $this->resource->getAttributes();
+        $a[$key] = $value;
+        $this->resource->setAttributes($a);
+    }
+
+    /**
+     * @param array $attributes dictionary of string key and values
+     * @return void
+     */
+    public function setAttributes(array $attributes) : void {
+        $a = $this->resource->getAttributes();
+        $a[] = $attributes;
+        $this->resource->setAttributes($a);
+    }
+
+    /**
+     * @param string $scope
+     * @return void
+     */
+    public function setScope(string $scope): void {
+        $this->resource->setScope($scope);
+    }
+}
---
+++ src/Sdk/Builder/ResourceAction.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Cerbos\Sdk\Builder;
+
+// Copyright 2021-2022 Zenauth Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+class ResourceAction
+{
+    private \Cerbos\Api\V1\Engine\ResourceAction $resourceAction;
+    private array $actions;
+
+    /**
+     * @param string $kind
+     * @param string $id
+     * @param array|null $actions
+     */
+    private function __construct(string $kind, string $id, ?array $actions)
+    {
+        $this->resourceAction = new \Cerbos\Api\V1\Engine\ResourceAction(new \Cerbos\Api\V1\Engine\Resource($id, $kind, null, null, null), array());
+        $this->actions = $actions;
+    }
+
+    /**
+     * @param string $kind
+     * @param string $id
+     * @param array|null $actions
+     * @return ResourceAction
+     */
+    public function newInstance(string $kind, string $id, ?array $actions): ResourceAction {
+        return new ResourceAction($kind, $id, $actions);
+    }
+
+    /**
+     * @param string $kind
+     * @return ResourceAction
+     */
+    public function withKind(string $kind): ResourceAction {
+        $this->resourceAction->setKind($kind);
+        return $this;
+    }
+
+    /**
+     * @param string $policyVersion
+     * @return ResourceAction
+     */
+    public function withPolicyVersion(string $policyVersion): ResourceAction {
+        $this->resourceAction->setPolicyVersion($policyVersion);
+        return $this;
+    }
+
+    /**
+     * @param string $key
+     * @param string $value
+     * @return $this
+     */
+    public function withAttribute(string $key, string $value): ResourceAction {
+        $a = $this->resourceAction->getAttributes();
+        $a[$key] = $value;
+        $this->resourceAction->setAttributes($a);
+        return $this;
+    }
+
+    /**
+     * @param array $attributes dictionary of string key and values
+     * @return $this
+     */
+    public function withAttributes(array $attributes): ResourceAction {
+        $a = $this->resourceAction->getAttributes();
+        $a[] = $attributes;
+        $this->resourceAction->setAttributes($a);
+        return $this;
+    }
+
+    /**
+     * @param string $scope
+     * @return $this
+     */
+    public function withScope(string $scope): ResourceAction {
+        $this->resourceAction->setScope($scope);
+        return $this;
+    }
+
+    /**
+     * @param string $action
+     * @return $this
+     */
+    public function withAction(string $action): ResourceAction {
+        $a = $this->actions;
+        $a[] = $action;
+        $this->actions = $a;
+        return $this;
+    }
+
+    /**
+     * @param array $actions
+     * @return $this
+     */
+    public function withActions(array $actions): ResourceAction {
+        $a = $this->actions;
+        $a[] = $actions;
+        $this->actions = $a;
+        return $this;
+    }
+}
